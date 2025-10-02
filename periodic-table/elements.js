d49a2fe4c9059937e91273bdf089db74a6064097
Steven <stevenhuynh.edu@gmail.com>
2025-10-02 12:24:33 -0500

    Add Bohr model visualization, atomic radius, electronegativity, and dark boxy UI to periodic table

---
+++ periodic-table/elements.js
@@ -0,0 +1,9 @@
+// elements.js
+// includes atomic radius/electronegativity for all elements
+const elements = [
+  { name: 'Hydrogen', atomicRadius: 53, electronegativity: 2.20 },
+  { name: 'Helium', atomicRadius: 31, electronegativity: null },
+  // Add other elements here
+];
+
+export default elements;
