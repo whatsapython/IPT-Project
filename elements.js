e1b30f182eff74c9cb07001cb07d8c6e767a9e01
Steven <stevenhuynh.edu@gmail.com>
2025-10-02 09:07:39 -0500

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,1 +1,12 @@
-
+// Will add more later
+const elements = [
+    {symbol: "H", name: "Hydrogen", number: 1, group: 1, period: 1, info: "Lightest element. Used in fuel cells."},
+    {symbol: "He", name: "Helium", number: 2, group: 18, period: 1, info: "Noble gas. Fills balloons."},
+    {symbol: "Li", name: "Lithium", number: 3, group: 1, period: 2, info: "Used in batteries."},
+    {symbol: "C", name: "Carbon", number: 6, group: 14, period: 2, info: "Basis of life. Found in all living things."},
+    {symbol: "O", name: "Oxygen", number: 8, group: 16, period: 2, info: "Needed for breathing."},
+    {symbol: "Na", name: "Sodium", number: 11, group: 1, period: 3, info: "Reacts with water."},
+    {symbol: "Cl", name: "Chlorine", number: 17, group: 17, period: 3, info: "Used for water purification."},
+    {symbol: "Fe", name: "Iron", number: 26, group: 8, period: 4, info: "Used for steel."}
+    // This will do for now
+];
