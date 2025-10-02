717e27079501386e3fb1690c4073c8521d41b0a4
Steven <stevenhuynh.edu@gmail.com>
2025-10-02 12:35:41 -0500

    Deleted template directory

--- periodic-table/elements.js
+++
@@ -1,9 +0,0 @@
-// elements.js
-// includes atomic radius/electronegativity for all elements
-const elements = [
-  { name: 'Hydrogen', atomicRadius: 53, electronegativity: 2.20 },
-  { name: 'Helium', atomicRadius: 31, electronegativity: null },
-  // Add other elements here
-];
-
-export default elements;
